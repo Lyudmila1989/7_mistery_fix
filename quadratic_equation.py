008dbf7258911d93390dce1b2edcd373d97cf0ce
lyudmila1989 <lyudmila_l_@mail.ru>
2018-01-19 07:00:22 +0700

    fixed comments

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,30 +1,31 @@
 from math import sqrt
 
+# Which version of function is better?
+# Which is more correct?
+# get_roots() or get_roots_s?
+
 
 def get_roots(a, b, c):
-    discriminant = b ** 2 - 4 * a * c              # discriminant calculation.
-    if discriminant < 0:   # If discriminant < 0 then there are not any roots.
+    discriminant = b ** 2 - 4 * a * c
+    if discriminant < 0:
         root1 = None
         root2 = None
-    elif discriminant == 0:  # If discriminant = 0 then there's one real root.
+    elif discriminant == 0:
         root1 = -b / (2 * a)
         root2 = None
-    else:                 # If discriminant > 0 then there are two real roots.
+    else:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
         root2 = (-b + sqrt(discriminant)) / (2 * a)
     return root1, root2
 
 
-# Over code version
-
-
 def get_roots_s(a, b, c):
-    discriminant = b ** 2 - 4 * a * c              # discriminant calculation.
-    if discriminant < 0:  # If discriminant < 0 then there are not real roots.
+    discriminant = b ** 2 - 4 * a * c
+    if discriminant < 0:
         return None, None
-    elif discriminant == 0:  # If discriminant = 0 then there's one real root.
+    elif discriminant == 0:
         return -b / (2 * a), None
-    else:                 # If discriminant > 0 then there are two real roots.
+    else:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
         root2 = (-b + sqrt(discriminant)) / (2 * a)
     return root1, root2
